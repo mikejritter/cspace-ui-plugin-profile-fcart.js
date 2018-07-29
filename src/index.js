4108145a9ea9aba50694451987f6253a6da7e0dc
Richard Millet <remillet@yahoo.com>
2018-07-28 18:05:15 -0700

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/* global cspaceUIPluginProfileFCart */
+/* The cspaceUIPluginProfileFCart global variable is set by webpack (in non-test builds). See
+ * webpack.config.js. */
+
+import { defineMessages } from 'react-intl';
+import messages from './messages';
+import plugins from './plugins';
+import styles from '../styles/cspace-ui-plugin-profile/fcart.css';
+
+module.exports = () => ({
+  messages,
+  className: styles.common,
+  prettyUrls: true,
+  tenantId: '1000',
+  pluginInfo: {
+    cspaceUIPluginProfileFCart: {
+      messages: defineMessages({
+        name: {
+          id: 'cspaceUIPluginProfileFCart.name',
+          defaultMessage: 'Fine and Contemporary Art profile',
+        },
+      }),
+      version: cspaceUIPluginProfileFCart.packageVersion,
+    },
+  },
+  plugins: plugins.map(plugin => plugin()),
+});

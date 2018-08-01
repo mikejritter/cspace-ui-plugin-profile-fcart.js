dae3cc4b8b328403d3239fb1b53e6a7b39bb3900
Richard Millet <remillet@yahoo.com>
2018-08-01 14:39:55 -0700

    DRYD-443: Hide the 'fieldCollectionEventName' and parent panel from FCart's acquisition UI plugin.

---
+++ src/plugins/recordTypes/acquisition/forms/default.jsx
@@ -0,0 +1,111 @@
+import { defineMessages } from 'react-intl';
+
+const template = (configContext) => {
+  const {
+    React,
+  } = configContext.lib;
+
+  const {
+    Panel,
+    Row,
+    Cols,
+    Col,
+  } = configContext.layoutComponents;
+
+  const {
+    Field,
+    InputTable,
+  } = configContext.recordComponents;
+
+  return (
+    <Field name="document">
+      <Panel name="info" collapsible>
+        <Cols>
+          <Col>
+            <Field name="acquisitionReferenceNumber" />
+            <Field name="accessionDateGroup" />
+
+            <InputTable name="acquisitionAuthorizer">
+              <Field name="acquisitionAuthorizer" />
+              <Field name="acquisitionAuthorizerDate" />
+            </InputTable>
+
+            <Field name="acquisitionDateGroupList">
+              <Field name="acquisitionDateGroup" />
+            </Field>
+
+            <Field name="acquisitionMethod" />
+
+            <Field name="acquisitionSources">
+              <Field name="acquisitionSource" />
+            </Field>
+
+            <Field name="owners">
+              <Field name="owner" />
+            </Field>
+
+            <Field name="transferOfTitleNumber" />
+          </Col>
+
+          <Col>
+            <Panel name="priceInformation">
+              <InputTable name="groupPurchasePrice">
+                <Field name="groupPurchasePriceCurrency" />
+                <Field name="groupPurchasePriceValue" />
+              </InputTable>
+
+              <InputTable name="objectOfferPrice">
+                <Field name="objectOfferPriceCurrency" />
+                <Field name="objectOfferPriceValue" />
+              </InputTable>
+
+              <InputTable name="objectPurchaseOfferPrice">
+                <Field name="objectPurchaseOfferPriceCurrency" />
+                <Field name="objectPurchaseOfferPriceValue" />
+              </InputTable>
+
+              <InputTable name="objectPurchasePrice">
+                <Field name="objectPurchasePriceCurrency" />
+                <Field name="objectPurchasePriceValue" />
+              </InputTable>
+
+              <InputTable name="originalObjectPurchasePrice">
+                <Field name="originalObjectPurchasePriceCurrency" />
+                <Field name="originalObjectPurchasePriceValue" />
+              </InputTable>
+            </Panel>
+
+            <Field name="acquisitionReason" />
+          </Col>
+        </Cols>
+
+        <Field name="acquisitionNote" />
+        <Field name="acquisitionProvisos" />
+
+        <Row>
+          <Field name="acquisitionFundingList">
+            <Field name="acquisitionFunding">
+              <Field name="acquisitionFundingCurrency" />
+              <Field name="acquisitionFundingValue" />
+              <Field name="acquisitionFundingSource" />
+              <Field name="acquisitionFundingSourceProvisos" />
+            </Field>
+          </Field>
+        </Row>
+
+        <Field name="creditLine" />
+      </Panel>
+
+    </Field>
+  );
+};
+
+export default configContext => ({
+  messages: defineMessages({
+    name: {
+      id: 'form.acquisition.default.name',
+      defaultMessage: 'Standard Template',
+    },
+  }),
+  template: template(configContext),
+});
--- src/plugins/recordTypes/index.js
+++ src/plugins/recordTypes/index.js
@@ -1,5 +1,7 @@
 import collectionobject from './collectionobject';
+import acquisition from './acquisition';
 
 export default [
   collectionobject,
+  acquisition,
 ];

069618fe17a47a5b3686f1b284ba99a57d2dd464
Richard Millet <remillet@yahoo.com>
2018-07-31 22:04:02 -0700

    DRYD-442: Fixed landing page message and bumped version number.

--- src/messages.js
+++ src/messages.js
@@ -1,2 +1,48 @@
 export default {
+  'about.title': 'Welcome to the CollectionSpace Fine and Contemporary Art Demo',
+  'about.contentHTML': `
+    <p>
+      CollectionSpace is a free, open-source collections management application for museums,
+      historical societies, natural science collections, and more.
+    </p>
+    <p>
+      This demo site is running the
+      <a href="http://www.collectionspace.org/current_release/">current version</a> the
+      <a href="https://wiki.collectionspace.org/x/xgDZCQ">fine and contemporary art</a> profile of
+      CollectionSpace, which includes fields and procedures useful for art collections. If
+      you’re interested in a version of CollectionSpace configured to meet the needs of a different
+      domain, please visit our <a href="http://demo.collectionspace.org">demo landing page</a>
+      to view all available options. Examples include Anthropology, Botanical Garden, Public Art,
+      Herbarium, and Local History &amp; Material Culture.
+    </p>
+    <p>
+      To log in, use the email <strong>admin@fcart.collectionspace.org</strong>, with the
+      case-sensitive password <strong>Administrator</strong>. To view in read-only mode, use the
+      email <strong>reader@fcart.collectionspace.org</strong> with the password
+      <strong>reader</strong>.
+    </p>
+    <p>
+      Not sure where to get started? Follow along with one of our quick-start guides:
+    </p>
+    <ul>
+      <li>
+        <span>Create a new object:</span>
+        <a href="http://bit.ly/newobjpdf">PDF</a>
+        <a href="https://vimeo.com/112212895">Screencast</a>
+      </li>
+      <li>
+        <span>Link to an image:</span>
+        <a href="http://bit.ly/linkimgpdf">PDF</a>
+        <a href="https://vimeo.com/112214418">Screencast</a>
+      </li>
+      <li>
+        <span>Add a storage location:</span>
+        <a href="http://bit.ly/storlocpdf">PDF</a>
+        <a href="https://vimeo.com/112818120">Screencast</a>
+      </li>
+    </ul>
+    <p>
+      We rebuild this demo site weekly so don't worry, you won't break it!
+    </p>
+  `,
 };

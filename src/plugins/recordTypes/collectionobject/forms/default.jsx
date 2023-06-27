320b150f45aafd8fd11e018a4c423b00a6908af8
Michael Ritter <mikejritter@users.noreply.github.com>
2023-06-26 18:28:32 -0600

    Add controlled fields to collectionobject (#14)

--- src/plugins/recordTypes/collectionobject/forms/default.jsx
+++ src/plugins/recordTypes/collectionobject/forms/default.jsx
@@ -108,6 +108,7 @@ const template = (configContext) => {
         <Field name="objectNameList">
           <Field name="objectNameGroup">
             <Field name="objectName" />
+            <Field name="objectNameControlled" />
             <Field name="objectNameCurrency" />
             <Field name="objectNameLevel" />
             <Field name="objectNameSystem" />
@@ -212,6 +213,7 @@ const template = (configContext) => {
         <Field name="materialGroupList">
           <Field name="materialGroup">
             <Field name="material" />
+            <Field name="materialControlled" />
             <Field name="materialComponent" />
             <Field name="materialComponentNote" />
             <Field name="materialName" />
@@ -310,6 +312,10 @@ const template = (configContext) => {
                   <Field name="contentEventName" />
                   <Field name="contentEventNameType" />
                 </Field>
+              </Field>
+
+              <Field name="contentEvents">
+                <Field name="contentEvent" />
               </Field>
 
               <Field name="contentOtherGroupList">
@@ -466,10 +472,9 @@ const template = (configContext) => {
                 <Field name="assocEventNameType" />
               </InputTable>
 
-              <InputTable name="assocControlledEvent">
+              <Field name="assocEvents">
                 <Field name="assocEvent" />
-                <Field name="assocEventType" />
-              </InputTable>
+              </Field>
 
               <Field name="assocEventOrganizations">
                 <Field name="assocEventOrganization" />

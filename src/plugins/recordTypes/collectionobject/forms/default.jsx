79280ed51cf03f5121a8b8c00c2f4303951b3486
Michael Ritter <mikejritter@users.noreply.github.com>
2024-11-08 13:34:14 -0700

    DRYD-1503: Production Place Verbatim (#21)

--- src/plugins/recordTypes/collectionobject/forms/default.jsx
+++ src/plugins/recordTypes/collectionobject/forms/default.jsx
@@ -153,6 +153,10 @@ const template = (configContext) => {
               </Field>
             </Field>
 
+            <Field name="objectProductionPlacesVerbatim" subpath="ns2:collectionobjects_objectprod_extension">
+              <Field name="objectProductionPlaceVerbatim" />
+            </Field>
+
             <Field name="objectProductionReasons">
               <Field name="objectProductionReason" />
             </Field>
